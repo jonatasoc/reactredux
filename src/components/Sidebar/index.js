dfded2875ae2e640a7d06cb6f7298acc6a220ae9
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2021-02-09 09:35:01 -0300

    Create Sidebar component render

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -21,4 +21,22 @@ export default class Sidebar extends Component {
       },
     ],
   };
+
+  render() {
+    const { modules } = this.state;
+    return (
+      <aside>
+        {modules.map((module) => (
+          <div key={module.id}>
+            <strong>{module.title}</strong>
+            <ul>
+              {module.lessons.map((lesson) => (
+                <li key={lesson.id}>{lesson.title}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </aside>
+    );
+  }
 }
