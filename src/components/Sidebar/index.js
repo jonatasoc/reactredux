2f710d76412f11ad17eb30a01b84edee038460e3
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2021-02-15 00:27:01 -0300

    Add Redux actions

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,42 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Sidebar extends Component {
-  state = {
-    modules: [
-      {
-        id: 1,
-        title: "Inciando com React",
-        lessons: [
-          { id: 1, title: "Primeira aula" },
-          { id: 2, title: "Segunda aula" },
-        ],
-      },
-      {
-        id: 2,
-        title: "Aprendendo Redux",
-        lessons: [
-          { id: 1, title: "Terceira aula" },
-          { id: 2, title: "Quarta aula" },
-        ],
-      },
-    ],
+import { connect } from "react-redux";
+
+function toggleLesson(module, lesson) {
+  return {
+    type: "TOGGLE_LESSON",
+    module,
+    lesson,
   };
-
-  render() {
-    const { modules } = this.state;
-    return (
-      <aside>
-        {modules.map((module) => (
-          <div key={module.id}>
-            <strong>{module.title}</strong>
-            <ul>
-              {module.lessons.map((lesson) => (
-                <li key={lesson.id}>{lesson.title}</li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </aside>
-    );
-  }
 }
+
+function Sidebar({ modules, dispatch }) {
+  return (
+    <aside>
+      {modules.map((module) => (
+        <div key={module.id}>
+          <strong>{module.title}</strong>
+          <ul>
+            {module.lessons.map((lesson) => (
+              <li key={lesson.id}>
+                {lesson.title}
+                <button onClick={() => dispatch(toggleLesson(module, lesson))}>
+                  Selecionar
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </aside>
+  );
+}
+
+export default connect(({ modules }) => ({
+  modules,
+}))(Sidebar);
--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,28 @@
 import { createStore } from "redux";
 
 function reducer() {
-  return [
-    {
-      id: 1,
-      title: "Inciando com React",
-      lessons: [
-        { id: 1, title: "Primeira aula" },
-        { id: 2, title: "Segunda aula" },
-      ],
-    },
-    {
-      id: 2,
-      title: "Aprendendo Redux",
-      lessons: [
-        { id: 1, title: "Terceira aula" },
-        { id: 2, title: "Quarta aula" },
-      ],
-    },
-  ];
+  return {
+    activeLesson: null,
+    activeModule: null,
+    modules: [
+      {
+        id: 1,
+        title: "Inciando com React",
+        lessons: [
+          { id: 1, title: "Primeira aula" },
+          { id: 2, title: "Segunda aula" },
+        ],
+      },
+      {
+        id: 2,
+        title: "Aprendendo Redux",
+        lessons: [
+          { id: 1, title: "Terceira aula" },
+          { id: 2, title: "Quarta aula" },
+        ],
+      },
+    ],
+  };
 }
 
 const store = createStore(reducer);
